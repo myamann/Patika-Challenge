0c9068677d62673838145b4918a3ffc4e0c87542
myamann <mehmetyaman331@gmail.com>
2021-04-05 17:52:59 +0300

    starter files todo part

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import ToDoList from './components/TodoList'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+      
+    </div>
+  );
+}
+
+export default App;
